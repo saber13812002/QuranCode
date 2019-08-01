e634b8b159a414f780712f099c362abf597d9d76
Ali Adams <heliwave@yahoo.com>
2019-08-01 12:21:37 +0300

    QuranCode 6.19.1259
    Fix Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,7 +1,18 @@
 @ECHO OFF
-IF EXIST "%WINDIR%\Microsoft.NET\Framework64\v4.*" COPY Files\NET4\*.* .
-GOTO END
-IF EXIST "%WINDIR%\Microsoft.NET\Framework\v4.*"   COPY Files\NET4\*.* .
-GOTO END
-                                                   COPY Files\NET2\*.* .
+IF EXIST "%WINDIR%\Microsoft.NET\Framework64\v4.*" (
+COPY Files\NET4\*.* .
+GOTO :END
+)
+IF EXIST "%WINDIR%\Microsoft.NET\Framework\v4.*" (
+COPY Files\NET4\*.* .
+GOTO :END
+)
+IF EXIST "%WINDIR%\Microsoft.NET\Framework64\v2.*" (
+COPY Files\NET2\*.* .
+GOTO :END
+)
+IF EXIST "%WINDIR%\Microsoft.NET\Framework\v2.*" (
+COPY Files\NET2\*.* .
+GOTO :END
+)
 :END
