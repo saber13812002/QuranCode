49e13faaf387be7c7e4e8b2b714d40c39133d85c
Ali Adams <heliwave@yahoo.com>
2019-12-06 23:56:36 +0300

    QuranCode 7.29.139.8317

--- Version.bat
+++ Version.bat
@@ -3,15 +3,15 @@ COPY Features.txt Build\Release\
 
 IF not exist "NET2" NET2.bat
 Tools\Replace\bin\Release\Replace.exe NET2 Version.bat      0000 8317
-Tools\Replace\bin\Release\Replace.exe NET2 Version.bat    00:00 07:29
+Tools\Replace\bin\Release\Replace.exe NET2 Version.bat    00:00 23:57
 
 Tools\Version\bin\Release\Version.exe .                 7.29.139.8317 7.29.139.8317 -Tools
 
-Tools\Touch\bin\Release\Touch.exe Build                         07:29               -Tools
-Tools\Touch\bin\Release\Touch.exe .                             07:29               -Tools
-Tools\Touch\bin\Release\Touch.exe Tools           2009-07-29    07:29
+Tools\Touch\bin\Release\Touch.exe Build                         23:57               -Tools
+Tools\Touch\bin\Release\Touch.exe .                             23:57               -Tools
+Tools\Touch\bin\Release\Touch.exe Tools           2009-07-29    23:57
 
-COPY QuranCode1433.zip          ..\Backup\QuranCode1433_7.29.139.0067.zip
-COPY QuranCode1433.Source.zip   ..\Backup\QuranCode1433_7.29.139.0067.Source.zip
+COPY QuranCode1433.zip          ..\Backup\QuranCode1433_7.29.139.8317.zip
+COPY QuranCode1433.Source.zip   ..\Backup\QuranCode1433_7.29.139.8317.Source.zip
 COPY QuranCode1433.zip          ..\Backup\
 COPY QuranCode1433.Source.zip   ..\Backup\
