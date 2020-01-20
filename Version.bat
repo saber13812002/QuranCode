6ea2f1bdda171011993ee0595d83c2eaede607ed
Ali Adams <heliwave@yahoo.com>
2020-01-20 19:19:09 +0300

    7.29.139.8317

--- Version.bat
+++ Version.bat
@@ -1,7 +1,7 @@
 COPY Readme.txt   Build\Release\
 COPY Features.txt Build\Release\
 
-IF not exist "NET2" NET2.bat
+IF NoT EXIST NET2\ NET2.bat
 Tools\Replace\bin\Release\Replace.exe NET2 Version.bat      0000 8317
 Tools\Replace\bin\Release\Replace.exe NET2 Version.bat    00:00 07:29
 
@@ -11,6 +11,7 @@ Tools\Touch\bin\Release\Touch.exe Build                         07:29          -
 Tools\Touch\bin\Release\Touch.exe .                             07:29          -Tools
 Tools\Touch\bin\Release\Touch.exe Tools      2009-07-29         07:29
 
+IF NOT EXIST ..\Backup\ MD ..\Backup
 COPY QuranCode1433.zip                   ..\Backup\QuranCode1433_7.29.139.zip
 COPY QuranCode1433.Source.zip            ..\Backup\QuranCode1433_7.29.139.Source.zip
 COPY QuranCode1433.zip                   ..\Backup\
